36d4bb7a21f28ddf9646f14234ee28f5e9c497e3
Evgeny <ebeniamin@yandex.ru>
2017-12-05 10:04:49 +0300

    Начальная стадия

---
+++ bootstrap/js/animation.js
@@ -0,0 +1,25 @@
+// Запускаем анимацию
+ onload=function (){  
+
+   $("#senoText").fadeIn(1000);
+  //$("#seno1").animate({left: '+=1300px'}, 2000);
+   
+
+   
+// Останавливаем анимацию
+   
+/* $("#stop").click(function(){
+   
+  $(".block").stop();
+   
+});
+   
+// Запускаем в другом направлении
+   
+$("#back").click(function(){
+   
+  $(".block").animate({left: '-=100px'}, 2000);
+   
+}); */
+
+}
